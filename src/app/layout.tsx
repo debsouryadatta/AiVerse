f88502b6ef8c254723c85f7c83cb6e1f9dae98d0
debsouryadatta <debsouryadatta@gmail.com>
2025-10-25 23:29:52 +0530

    minor fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/common/Providers";
 import { Toaster } from "@/components/ui/sonner";
-import Footer from "@/components/common/Footer";
-import { cn } from "@/lib/utils";
 import { Analytics } from "@/components/common/Analytics";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -12,6 +10,33 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "AiVerse",
   description: "Unleash the Power of AI in Learning. Join the Community!",
+  openGraph: {
+    type: "website",
+    title: "AiVerse",
+    description: "Unleash the Power of AI in Learning. Join the Community!",
+    url: "https://aiverse.souryax.tech",
+    siteName: "AiVerse",
+    images: [{
+      url: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734083939/projects/Screenshot_2024-12-13_at_3.24.58_PM_nsld3o.png",
+      width: 1200,
+      height: 630,
+      alt: "AiVerse Preview Image",
+    }],
+    locale: "en_US",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "AiVerse",
+    description: "Unleash the Power of AI in Learning. Join the Community!",
+    site: "https://aiverse.souryax.tech",
+    images: [{
+      url: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734083939/projects/Screenshot_2024-12-13_at_3.24.58_PM_nsld3o.png",
+      alt: "AiVerse Preview Image",
+    }],
+  },
+  icons: {
+    shortcut: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734098503/projects/aiverse-logo_mbtjg8.png",
+  }
 };
 
 export default function RootLayout({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { BenefitsSection } from "@/components/landing/layout/sections/benefits";
 import { FAQSection } from "@/components/landing/layout/sections/faq";
 import { FeaturesSection } from "@/components/landing/layout/sections/features";
@@ -6,44 +8,18 @@ import { HeroSection } from "@/components/landing/layout/sections/hero";
 import { PricingSection } from "@/components/landing/layout/sections/pricing";
 import { SponsorsSection } from "@/components/landing/layout/sections/sponsors";
 import { Navbar } from "@/components/landing/layout/navbar";
-import { auth } from "@/lib/auth";
-import { redirect } from "next/navigation";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
 
-export const metadata = {
-  title: "AiVerse",
-  description: "Unleash the Power of AI in Learning. Join the Community!",
-  openGraph: {
-    type: "website",
-    title: "AiVerse",
-    description: "Unleash the Power of AI in Learning. Join the Community!",
-    url: "https://aiverse.souryax.tech",
-    siteName: "AiVerse",
-    images: [{
-      url: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734083939/projects/Screenshot_2024-12-13_at_3.24.58_PM_nsld3o.png",
-      width: 1200,
-      height: 630,
-      alt: "AiVerse Preview Image",
-    }],
-    locale: "en_US",
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: "AiVerse",
-    description: "Unleash the Power of AI in Learning. Join the Community!",
-    site: "https://aiverse.souryax.tech",
-    images: [{
-      url: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734083939/projects/Screenshot_2024-12-13_at_3.24.58_PM_nsld3o.png",
-      alt: "AiVerse Preview Image",
-    }],
-  },
-  icons: {
-    shortcut: "https://res.cloudinary.com/diyxwdtjd/image/upload/v1734098503/projects/aiverse-logo_mbtjg8.png",
-  }
-};
+export default function Home() {
+  const session = useSession();
 
-export default async function Home() {
-  const session = await auth();
-  if (session?.user?.id) redirect("/explore");
+  useEffect(() => {
+    if (session?.data?.user) {
+      window.location.href = "/explore";
+    }
+  }, [session])
+  
 
   return (
     <>
